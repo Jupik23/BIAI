982e13e084fb7909b97933a315729390f1d8ae33
Jupik23 <jupekkekw@gmail.com>
2025-04-26 16:31:55 +0200

    path checker

--- data/dataloader.py
+++ data/dataloader.py
@@ -3,10 +3,17 @@ from torch.utils.data import DataLoader
 import os
 
 def get_dataloaders(data_dir='chest_xray', batch_size=16, num_workers=2):
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    data_path = os.path.join(base_dir, data_dir)
+
+    if not os.path.exists(data_path):
+        raise FileNotFoundError(f"Nie znaleziono katalogu z danymi: {data_path}")
+
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
     ])
+
 
     train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), transform=transform)
     val_dataset = datasets.ImageFolder(os.path.join(data_dir, 'val'), transform=transform)
